bb62be06d6c76e8828c304c95059c7c13f993db8
Dulaj Kavinda <dulajkaavinda@gmail.com>
2020-12-18 21:20:29 +0530

    stories bloc

---
+++ lib/src/blocs/stories_provider.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'stories_bloc.dart';
+
+class StoriesProvider extends InheritedWidget {
+  final StoriesBloc bloc;
+
+  StoriesProvider({Key key, Widget child})
+      : bloc = StoriesBloc(),
+        super(child: child, key: key);
+
+  @override
+  bool updateShouldNotify(_) => true;
+
+  static StoriesBloc of(BuildContext context) {
+    return (context.dependOnInheritedWidgetOfExactType<StoriesProvider>()).bloc;
+  }
+}
